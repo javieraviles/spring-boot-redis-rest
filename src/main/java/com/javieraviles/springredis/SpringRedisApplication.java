3026bb87e24b0bf62204e09991102afc53861987
Javier Aviles <avileslopez.javier@gmail.com>
2018-08-23 17:45:57 +0200

    Spring boot base from official initializr

---
+++ src/main/java/com/javieraviles/springredis/SpringRedisApplication.java
@@ -0,0 +1,12 @@
+package com.javieraviles.springredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(SpringRedisApplication.class, args);
+	}
+}
