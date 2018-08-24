d12d99a505e652479b300f8500c13a898ad5012e
Aviles, Javier <franciscojavier.avileslopez@nttdata.com>
2018-08-24 12:48:48 +0200

    Adding ID attribute to user entity

--- src/main/java/com/javieraviles/springredis/entities/User.java
+++ src/main/java/com/javieraviles/springredis/entities/User.java
@@ -6,9 +6,19 @@ public class User implements Serializable {
 
 	private static final long serialVersionUID = -305726463442998985L;
 
+	private String id;
+
 	private String name;
 
 	private String email;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(final String id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
